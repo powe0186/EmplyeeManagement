2abba051d2e905a8aa5332019789be4853dd5acb
Ben Powell <powe0186@gmail.com>
2022-04-25 16:36:15 -0500

    add departments query works.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
 var inquirer = require('inquirer');
-const { mainMenuQuestion } = require('./lib/questions');
+const { mainMenuQuestion, addDepartmentQuestion } = require('./lib/questions');
 //const { mainMenuInquirer } = require('./lib/mainMenuInquirer');
-const { allDepts, allRoles } = require('./lib/queries');
+const { allDepts, allRoles, allEmployees, addDepartment } = require('./lib/queries');
 
 // Connect to database
 
@@ -16,32 +16,33 @@ inquirer.prompt(mainMenuQuestion)
         console.error(err);
         return err;
     })
+
     .then((choice) => {
         switch (choice) {
             case 'View all departments.':
                 //Get the entire departments table.
                 allDepts();
                 break;
-        
+
             case 'View all roles.':
                 //get the entire roles table
                 allRoles();
                 break;
 
-                case 'View all employees.':
-                    //get the entire employees table.
-                    db.query('SELECT * FROM employees', (err, results) => {
-                        if (err) {
-                            console.log("sorry, there was an error.")
-                        } else {
-                            console.log(results);
-                        }
-                    })
-                    break;
+            case 'View all employees.':
+                //get the entire employees table.
+                allEmployees();
+                break;
 
-                // case 'Add a department':
-                //     // use inquirer to ask which department. Use that for query
-                //     break;
+            case 'Add a department.':
+                // use inquirer to ask which department. Use that for query
+                inquirer.prompt(addDepartmentQuestion)
+                .then((ans) => {
+                    addDepartment(ans.newDept);
+                    console.log(`${ans.newDept} added to departments table.`)
+                })
+                .catch((err) => console.error(err));
+                break;
         }
     })
 
