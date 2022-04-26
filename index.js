c77f6824dac758143f61ed630f5a264f32ebaa8d
Ben Powell <powe0186@gmail.com>
2022-04-25 23:00:15 -0500

    Add Role COmpletely words (although it needs to be refactored and modularized

--- index.js
+++ index.js
@@ -44,19 +44,61 @@ inquirer.prompt(mainMenuQuestion)
                 break;
 
             case 'Add a role.':
-                // use inquirer to ask which department. Use that for query
-                addRoleQuestions()
-                .then((questions) => {
-                inquirer.prompt(questions)})
-                .then((ans) =>{
-                    addRole(ans.role, ans.salary, getDeptId(ans.dept))
-                });
-                break;
-        }
-    })
+                // use inquirer to ask which department. Use that for quer
+                let role;
+                let salary;
+                allDepts()
+                    .then(([results, fields]) => {
+                        let possibleDepts = [];
+                        for (let i = 0; i < results.length; i++) {
+                            possibleDepts.push(results[i].name);
+                        }
+                        return possibleDepts;
+                    })
+                    .then((possibleDepts) => {
+                        let questions = [
+                            {
+                                type: 'input',
+                                name: 'role',
+                                message: 'What is the name of the new role?'
+                            },
+                            {
+                                type: 'number',
+                                name: 'salary',
+                                message: 'What is the salary of the new Role?'
+                            },
+                            {
+                                type: 'list',
+                                choices: possibleDepts,
+                                name: 'dept',
+                                message: 'In what department will your new role work?'
+                            }
+                        ];
 
+                        return questions;
+                    })
+                    .then((questions) => {
+                        inquirer.prompt(questions)
+                            .then((answers) => {
+                                role = answers.role;
+                                salary = answers.salary;
+                                return answers.dept;
+                            })
+                            .then((dept) => {
+                                getDeptId(dept)
+                                .then((dept_id) => {
+                                    console.log(dept_id[0]);
+                                    addRole(role, salary, dept_id[0][0].id);
+                                    return;
+                            })
+                            
+                            })
+                        
+                    })
+                    break;
+    }
+})
 
-console.log('Await worked');
 
 
 // db.query('SELECT id,first_name FROM students', function (err, results) {
