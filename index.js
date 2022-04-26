3556a328712ad9511509f838b946fdc5a92a19d5
Ben Powell <powe0186@gmail.com>
2022-04-25 23:53:59 -0500

    Simplified adding roles and employees

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
 var inquirer = require('inquirer');
-const { mainMenuQuestion, addDepartmentQuestion, addRoleQuestions } = require('./lib/questions');
+const { mainMenuQuestion, addDepartmentQuestion, roleQuestions, employeeQuestions } = require('./lib/questions');
 //const { mainMenuInquirer } = require('./lib/mainMenuInquirer');
-const { allDepts, allRoles, allEmployees, addDepartment, addRole, getDeptId } = require('./lib/queries');
+const { allDepts, allRoles, allEmployees, addDepartment, addRole, addEmployee } = require('./lib/queries');
 
 // Connect to database
 inquirer.prompt(mainMenuQuestion)
@@ -39,65 +39,31 @@ inquirer.prompt(mainMenuQuestion)
                     .then((ans) => {
                         addDepartment(ans.newDept);
                         console.log(`${ans.newDept} added to departments table.`)
+                        return;
                     })
                     .catch((err) => console.error(err));
                 break;
 
             case 'Add a role.':
-                // use inquirer to ask which department. Use that for quer
-                let role;
-                let salary;
-                allDepts()
-                    .then(([results, fields]) => {
-                        let possibleDepts = [];
-                        for (let i = 0; i < results.length; i++) {
-                            possibleDepts.push(results[i].name);
-                        }
-                        return possibleDepts;
+                inquirer.prompt(roleQuestions)
+                    .then((answers) => {
+                        addRole(answers.role, answers.salary, answers.dept);
+                        return;
                     })
-                    .then((possibleDepts) => {
-                        let questions = [
-                            {
-                                type: 'input',
-                                name: 'role',
-                                message: 'What is the name of the new role?'
-                            },
-                            {
-                                type: 'number',
-                                name: 'salary',
-                                message: 'What is the salary of the new Role?'
-                            },
-                            {
-                                type: 'list',
-                                choices: possibleDepts,
-                                name: 'dept',
-                                message: 'In what department will your new role work?'
-                            }
-                        ];
-
-                        return questions;
+                    .catch((err) => console.error(err));
+                break;
+            
+            case 'Add an employee.':
+                inquirer.prompt(employeeQuestions)
+                    .then((answers) => {
+                        addEmployee(answers.first, answers.last, answers.roleId, answers.managerId);
+                        return;
                     })
-                    .then((questions) => {
-                        inquirer.prompt(questions)
-                            .then((answers) => {
-                                role = answers.role;
-                                salary = answers.salary;
-                                return answers.dept;
-                            })
-                            .then((dept) => {
-                                getDeptId(dept)
-                                .then((dept_id) => {
-                                    console.log(dept_id[0]);
-                                    addRole(role, salary, dept_id[0][0].id);
-                                    return;
-                            })
-                            
-                            })
-                        
-                    })
-                    break;
-    }
-})
+                    .catch((err) => console.error(err));
+                break
+                
+        }
+    })
 
 
 
